74d043534442f278d6bf75629cc26fae3ffed255
Vitalik Melnyk <melnyk.vitalikk@gmail.com>
2020-04-22 21:51:28 +0300

    update package.json with concurrently

--- backend/app/index.js
+++ backend/app/index.js
@@ -24,15 +24,14 @@ app.use(login);
 app.use(users);
 app.use(rooms);
 
-// app.use(express.static(path.join(__dirname, "client", "build")));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-// });
-
-app.use(express.static("client/build"));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const server = app.listen(port, () =>
   console.log(`Example app listening on port ${port}!`)
