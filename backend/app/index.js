550f154857dbfc85958b599fa8f776a99f37e587
Vitalik Melnyk <melnyk.vitalikk@gmail.com>
2020-04-22 21:22:31 +0300

    change  static path

--- backend/app/index.js
+++ backend/app/index.js
@@ -24,12 +24,15 @@ app.use(login);
 app.use(users);
 app.use(rooms);
 
-// if (process.env.NODE_ENV === "production") {
-app.use(express.static(path.join(__dirname, "client", "build")));
+// app.use(express.static(path.join(__dirname, "client", "build")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
+
+app.use(express.static("client/build"));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
-// }
 
 const server = app.listen(port, () =>
   console.log(`Example app listening on port ${port}!`)
