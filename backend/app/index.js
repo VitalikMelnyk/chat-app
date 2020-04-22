51cc59abd6b8e51280450e5c85d1b28e686e9b21
Vitalik Melnyk <melnyk.vitalikk@gmail.com>
2020-04-22 20:39:09 +0300

    update package.json

--- backend/app/index.js
+++ backend/app/index.js
@@ -24,12 +24,12 @@ app.use(login);
 app.use(users);
 app.use(rooms);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("/client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+app.use(express.static(path.join(__dirname, "client", "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+// }
 
 const server = app.listen(port, () =>
   console.log(`Example app listening on port ${port}!`)
