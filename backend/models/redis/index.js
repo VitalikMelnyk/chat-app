1da95f351f489f6a59f54438ee70ed3e0d04ed74
Vitalik Melnyk <melnyk.vitalikk@gmail.com>
2020-04-22 21:04:52 +0300

    update redis config

--- backend/models/redis/index.js
+++ backend/models/redis/index.js
@@ -1,35 +1,32 @@
 const redis = require("redis");
-const REMOTE_REDIS_ENDPOINT =
-  "redis-12204.c89.us-east-1-3.ec2.cloud.redislabs.com";
-
-const REMOTE_REDIS_PORT = 12204;
-const REMOTE_REDIS_AUTH = "8qmM0vDZM46eOiWqgqOc9Hee78R82Tf4";
-// const redisClient = redis.createClient({ host: "127.0.0.1", port: 6379 });
-const redisClient = redis.createClient({
-  host: REMOTE_REDIS_ENDPOINT,
-  port: REMOTE_REDIS_PORT,
-  // password: "gxybXLWMuhhjlZX6zoTOEDjHJViqsacQ",
+const url = require('url');
+const redisURL = url.parse(process.env.REDISCLOUD_URL);
+const redisClient = redis.createClient(redisURL.port, redisURL.hostname, {
+  no_ready_check: true,
 });
+redisClient.auth(redisURL.auth.split(":")[1]);
+// const REMOTE_REDIS_ENDPOINT =
+//   "redis-12204.c89.us-east-1-3.ec2.cloud.redislabs.com";
+// const REMOTE_REDIS_PORT = 12204;
+// const REMOTE_REDIS_AUTH = "8qmM0vDZM46eOiWqgqOc9Hee78R82Tf4";
+// // const redisClient = redis.createClient({ host: "127.0.0.1", port: 6379 });
+// const redisClient = redis.createClient({
+//   host: REMOTE_REDIS_ENDPOINT,
+//   port: REMOTE_REDIS_PORT,
+//   // password: "gxybXLWMuhhjlZX6zoTOEDjHJViqsacQ",
+// });
 
-redisClient.auth(
-  process.env.REDISCLOUD_URL || REMOTE_REDIS_AUTH,
-  (err, res) => {
-    if (err) {
-      throw err;
-    } else {
-      console.log(res);
-    }
-  }
-);
+// redisClient.auth(
+//   process.env.REDISCLOUD_URL || REMOTE_REDIS_AUTH,
+//   (err, res) => {
+//     if (err) {
+//       throw err;
+//     } else {
+//       console.log(res);
+//     }
+//   }
+// );
 
-// redisClient.set("Football", "Messi");
-redisClient.get("5e8469d769c20f4538a9920d", function (err, response) {
-  if (err) {
-    throw err;
-  } else {
-    console.log(response);
-  }
-});
 // redisClient.on("connect", () => {
 //   console.log("Redis client connected");
 // });
